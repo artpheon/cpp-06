68e36330c59577133b5781777b89ae96b53867ef
hrobbin <idcdtokms@gmail.com>
2021-07-02 16:47:52 +0300

    changed ex01: added fsanitize, casts, etc. everything works

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,7 +1,5 @@
 #include "Data.hpp"
-#include <fstream>
 #include <iostream>
-#include <sstream>
 
 /*          ВАРИАНТ 1
 uintptr_t serialize(Data* ptr) {
@@ -14,14 +12,39 @@ Data* deserialize(uintptr_t raw) {
 
 //          ВАРИАНТ 2
 uintptr_t serialize(Data* ptr) {
-    uintptr_t result = static_cast<>
+    ptr->_int = 10;
+    ptr->_str1 = "string1";
+    ptr->_str2 = "string2";
+    void* result = reinterpret_cast<void *>(ptr);
+    return reinterpret_cast<uintptr_t>(result);
 }
 Data* deserialize(uintptr_t raw) {
-    return reinterpret_cast<Data *>(raw);
+    Data* res = new Data;
+    void* obj = reinterpret_cast<void *>(raw);
+    res->_int = *(reinterpret_cast<int *>(obj));
+    res->_str1 = std::string(reinterpret_cast<char *>(obj) + 9, 7);
+    res->_str2 = std::string(reinterpret_cast<char *>(obj) + 33, 7);
+
+    return res;
 }
+
 int main() {
     Data data;
     uintptr_t ser = serialize(&data);
-    std::cout << sizeof(uintptr_t);
+    std::cout << "\nSerialized value = " << ser << std::endl;
+    std::cout << "Initial data:" << std::endl;
+    std::cout << data._int << std::endl;
+    std::cout << data._str1 << std::endl;
+    std::cout << data._str2 << std::endl << std::endl;
+    Data* sameData = deserialize(ser);
+    std::cout << "Size of Data = " << sizeof(data) 
+    << "\nSize of int = " << sizeof(data._int)
+    << "\nSize of Str1 = " << sizeof(data._str1)
+    << "\nSize of Str2 = " << sizeof(data._str2)
+    << "\nSize of uintptr = " << sizeof(uintptr_t)
+    << "\n\nResult data:" << std::endl;
+    std::cout << sameData->_int << std::endl;
+    std::cout << sameData->_str1 << std::endl;
+    std::cout << sameData->_str2 << std::endl;
     return 0;
 }
